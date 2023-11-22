63079f41f3465375b51a60ae8d46ba3c558e4744
jwblau2 <jeremy@abundantly.com>
2023-11-21 19:03:09 -0700

    added to the main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.jsx'
-import './index.css'
+import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
+import Checkout from './pages/Checkout.jsx'
+import Cart from './pages/Cart.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Signup />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/cart',
+        element: <Cart />
+      }, {
+        path: '/checkout',
+        element: <Checkout />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
