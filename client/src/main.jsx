0a1b9edabd3d1f52f7562ade910fd6548bc16854
Jake Provard <provardjake@gmail.com>
2023-11-21 19:40:47 -0700

    commented out pages and components that were causing errors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,29 +1,37 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App.jsx'
 import Signup from './pages/Signup.jsx'
 import Login from './pages/Login.jsx'
 import Checkout from './pages/Checkout.jsx'
 import Cart from './pages/Cart.jsx'
+import ErrorPage from "./pages/ErrorPage.jsx"
+import Home from "./pages/Home.jsx"
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    errorElement: <ErrorPage/>,
     children: [
       {
         index: true,
+        element: <Home />
+      }, 
+      {
+        path: '/signup',
         element: <Signup />
-      }, {
+      },
+      {
         path: '/login',
         element: <Login />
-      }, {
+      }, 
+      {
         path: '/cart',
         element: <Cart />
-      }, {
+      }, 
+      {
         path: '/checkout',
         element: <Checkout />
       }
