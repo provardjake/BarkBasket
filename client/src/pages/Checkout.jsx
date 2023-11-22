559a14dd30e05da89ff696610d18d34c8091f8a6
Joe Nadherny <joenadherny@gmail.com>
2023-11-21 19:47:42 -0700

    checkout jsx

---
+++ client/src/pages/Checkout.jsx
@@ -0,0 +1,12 @@
+const Checkout = () =>{
+    return (
+        <div>
+          <h2>Order Shipped!</h2>
+          <p>Your order has been successfully shipped. Thank you for shopping with us!</p>
+          {/* You can include additional order details or relevant information here */}
+        </div>
+      );
+    };
+
+
+export default Checkout;
