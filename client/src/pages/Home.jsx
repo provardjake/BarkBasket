9a998d977e05ab364ddc050ebb42bbe3478a0d35
jwblau2 <jeremy@abundantly.com>
2023-11-29 09:06:12 -0700

    updated the home page to our color scheme and updated Add to cart button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,39 +1,45 @@
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { QUERY_PRODUCTS } from '../utils/queries';
+import './Home.css'; // Import the Home.css file
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
   const products = data?.products || [];
+  const [addedToCart, setAddedToCart] = useState({});
 
-  console.log(products);
+  const handleAddToCart = (productId) => {
+    setAddedToCart({ ...addedToCart, [productId]: true });
+    // You can add further functionality related to adding to the cart here
+  };
 
   return (
-    <main>
+    <main className="main-container">
       <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-        </div>
+        {/* Other content */}
         <div className="col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
-          products &&
+            products &&
             products.map((product) => (
-              <Card style={{ width: '18rem' }} key= {product._id}>
-              <Card.Img variant="top" src={product.image} />
-              <Card.Body>
-                <Card.Title>{product.name}</Card.Title>
-                <Card.Text>
-                  {product.price}
-                </Card.Text>
-                <Button variant="primary">Add To Cart</Button>
-              </Card.Body>
-            </Card>
-        ))
+              <Card className="product-card" key={product._id}>
+                <Card.Img variant="top" src={product.image} />
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Text>{product.price}</Card.Text>
+                  <Button
+                    variant="primary"
+                    onClick={() => handleAddToCart(product._id)}
+                    disabled={addedToCart[product._id]}
+                  >
+                    {addedToCart[product._id] ? 'Added to Cart' : 'Add To Cart'}
+                  </Button>
+                </Card.Body>
+              </Card>
+            ))
           )}
         </div>
       </div>
